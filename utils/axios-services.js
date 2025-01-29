fdffcbdfc6338fc98945f0fe81397db80025b4b2
Mahmood-Ahmad-75 <mahmoodraj75@gmail.com>
2025-01-29 16:25:30 +0500

    email recovery component

---
+++ utils/axios-services.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// const BASE_URL = 'https://neta-dev-xi.vercel.app/api/';
+const BASE_URL = 'http://192.168.1.105:3000/api/';
+const apiClient = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+});
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error('Error retrieving token:', error);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// apiClient.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response && error.response.status === 401) {
+//       console.error('Unauthorized! Redirect to login.');
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+export default apiClient;
