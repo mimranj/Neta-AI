90cda3c75a0fe88208331851c48ecce13f494d5b
Mahmood-Ahmad-75 <mahmoodraj75@gmail.com>
2025-01-30 12:56:00 +0500

    changes in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,10 +7,7 @@ export default function RootLayout() {
       <Stack.Screen name="+not-found" />
       <Stack.Screen name="signup" />
       <Stack.Screen name="login" />
-      {/* <Stack.Screen name="home" />
-      <Stack.Screen name="profile" />
-      <Stack.Screen name="editprofile" />
-      <Stack.Screen name="subscription" /> */}
+     
     </Stack>
   );
 }
