648dc62e7ff13518d555f7b958ca411487f3951e
M_Imran <mimranjafar8@gmail.com>
2025-01-30 15:09:22 +0500

    routing fixes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,10 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
-      <Stack.Screen name="+not-found" />
-      <Stack.Screen name="signup" />
-      <Stack.Screen name="login" />
-     
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
     </Stack>
   );
 }
